3091423450a84a37d97076224a91ac53b972154b
moshe polter <44886334+moshepolter@users.noreply.github.com>
2019-04-24 07:59:24 -0700

    put javaScript in separate file/claj.js

---
+++ calj.js
@@ -0,0 +1,24 @@
+  //amount of times clicked
+let count=0
+let val1 = ""
+let val2 = ""
+//function runs when any number is clicked
+function number(number_value){
+    set_number_value(number_value, val1)
+    console.log(number_value)
+}
+//clears whatever was clicked
+//   let final = document.getElementById("ei").innerHTML = val1
+//take in number
+//if numberString is more than 10 digits long
+//add the number to the end of a number string
+
+function set_number_value(number, value){
+    if(value.length < 10){
+        value = value + number
+    }
+    //console.log(number)
+}
+function c(){
+    window.location("file:///C:/Users/moshe_utzfyrr/OneDrive/Documents/md.html")
+}
